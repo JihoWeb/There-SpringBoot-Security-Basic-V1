d84dc37e06eb2578486d9dccc0305515671b4eb7
Jihogrammer <jiho@kakao.com>
2021-09-15 09:41:07 +0900

    initial commit

---
+++ src/main/java/there/spring/security/ThereSecurityBasicV1Application.java
@@ -0,0 +1,13 @@
+package there.spring.security;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThereSecurityBasicV1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ThereSecurityBasicV1Application.class, args);
+	}
+
+}
---
+++ src/test/java/there/spring/security/ThereSecurityBasicV1ApplicationTests.java
@@ -0,0 +1,13 @@
+package there.spring.security;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThereSecurityBasicV1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
